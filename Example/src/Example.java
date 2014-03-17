07b2355d123424407b27af1dd67622fb6b96dea2
Naveen Kumar <miryalan95@gmail.com>
2014-03-17 10:45:21 -0500

    Example

---
+++ Example/src/Example.java
@@ -0,0 +1,13 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		int num;
+		num=100;
+		System.out.println("this is num"    +num);
+		num=num*2;
+		System.out.println("the value of num is"    +num);
+
+	}
+
+}
